cae30a366b4f6f2e265f57ed657b2c6b0423393e
Carole Aymon <carole.aymon@gmail.com>
2016-12-01 21:21:04 +0100

    add sourcemp and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,37 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+
+
 gulp.task('sass', function () {
   return gulp.src('css/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+  .pipe(sourcemaps.init())
+  .pipe(sass()
+
+  .on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(autoprefixer({
+             browsers: ['last 2 versions'],
+             cascade: false
+         }))
+  .pipe(gulp.dest('./css'));
 });
- 
+
 gulp.task('sass:watch', function () {
   gulp.watch('css/style.scss', ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
